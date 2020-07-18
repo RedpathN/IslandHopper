be56ee30954ba8f0219b5a5d0f5c8707cfbbaee3
Maxwell Clarke <maxeonyx@gmail.com>
2020-07-19 09:59:58 +1200

    Add palm trees and make Terrain Prefab

---
+++ Assets/Quaternius/Randomize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Randomize : MonoBehaviour
+{
+
+    public bool IsRandomized = false;
+
+    public List<GameObject> prefabs = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!IsRandomized) {
+            var index = Random.Range(0, prefabs.Count);
+            var tree = Instantiate(prefabs[index], transform);
+
+            var scale = Random.Range(1f, 1.7f);
+            tree.transform.localScale *= scale;
+            tree.transform.Rotate(new Vector3(0, 0, 1), Random.Range(0f, 360f));
+
+            IsRandomized = true;
+        }
+    }
+}
