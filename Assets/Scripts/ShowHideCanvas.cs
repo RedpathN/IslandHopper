d5f2eb46467d19486eca82866a401351f834c70c
Kilreth <jonathan.pavat.brown@gmail.com>
2020-07-18 21:11:33 +1200

    Add main menu scene
    The instructions text still needs to be written, and the Play button linked to the first island scene

---
+++ Assets/Scripts/ShowHideCanvas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Assign this to the Canvas, and call SetState() for every menu transition.
+/// </summary>
+public class ShowHideCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private string defaultState;
+
+    void Awake()
+    {
+        SetState(defaultState);
+    }
+
+    /// <summary>
+    /// Ask each UI object to show or hide according to the new state.
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetState(string state)
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            ShowHideUI showHideUI = child.gameObject.GetComponent<ShowHideUI>();
+            if (showHideUI != null)
+            {
+                showHideUI.ShowOrHide(state);
+            }
+        }
+    }
+}
