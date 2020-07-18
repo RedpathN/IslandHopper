69b651b50954edb340be1f6c983216be9c842cea
vYvNickvYv <redpathn@gmail.com>
2020-07-18 23:33:33 +1200

    Wooter

---
+++ Assets/Scripts/SetColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject water;
+    Renderer rend;
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();//.material.SetVector("_Color", new Vector4(0, 0.2f, 0.7f, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        {
+
+        }
+    }
+}
