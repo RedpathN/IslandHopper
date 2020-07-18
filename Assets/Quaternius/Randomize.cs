137da96d89029f3aa513ea98569100534a5e3c23
Maxwell Clarke <maxeonyx@gmail.com>
2020-07-19 10:36:24 +1200

    Add rocks to Islands 1 & 2

--- Assets/Quaternius/Randomize.cs
+++ Assets/Quaternius/Randomize.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Randomize : MonoBehaviour
 {
-
+    public bool RotateAroundYAxis = true;
     public bool IsRandomized = false;
+    public bool ScaleAxesSeparately = false;
+
+    public Vector2 XScaleRange = new Vector2(1, 2);
+    public Vector2 YScaleRange = new Vector2(1, 2);
+    public Vector2 ZScaleRange = new Vector2(1, 2);
 
     public List<GameObject> prefabs = new List<GameObject>();
 
@@ -15,11 +20,30 @@ public class Randomize : MonoBehaviour
     {
         if (!IsRandomized) {
             var index = Random.Range(0, prefabs.Count);
-            var tree = Instantiate(prefabs[index], transform);
+            var subObject = Instantiate(prefabs[index], transform);
 
-            var scale = Random.Range(1f, 1.7f);
-            tree.transform.localScale *= scale;
-            tree.transform.Rotate(new Vector3(0, 0, 1), Random.Range(0f, 360f));
+            var xscale = Random.Range(XScaleRange.x, XScaleRange.y);
+            var yscale = Random.Range(YScaleRange.x, YScaleRange.y);
+            var zscale = Random.Range(ZScaleRange.x, ZScaleRange.y);
+            Vector3 scaleVector;
+            if (ScaleAxesSeparately)
+            {
+                scaleVector = new Vector3(xscale, yscale, zscale);
+            }
+            else
+            {
+                scaleVector = new Vector3(xscale, xscale, xscale);
+            }
+            subObject.transform.localScale = new Vector3(subObject.transform.localScale.x * scaleVector.x, subObject.transform.localScale.y * scaleVector.y, subObject.transform.localScale.z * scaleVector.z);
+
+            // Quaternius assets are rotated around Z axis...
+            var rotateAxis = new Vector3(0, 0, 1);
+            if (RotateAroundYAxis)
+            {
+                rotateAxis = new Vector3(0, 1, 0);
+            }
+
+            subObject.transform.Rotate(rotateAxis, Random.Range(0f, 360f));
 
             IsRandomized = true;
         }
