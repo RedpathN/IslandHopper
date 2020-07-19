e1dca572161e338917e4d368611a8c129916cf05
Grace <gh971@nyu.edu>
2020-07-19 16:28:28 +1200

    remove some debug.logs

--- Assets/PickupItem.cs
+++ Assets/PickupItem.cs
@@ -20,85 +20,87 @@ public class PickupItem : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
-
-             gameObject.GetComponent<KeyPrompt>().Show();
-
-        if (this.gameObject.tag == "Platform")
         {
-            Debug.Log("On the platform");
-            other.GetComponent<PlayerController>().onPlatform = true;
-            if (Input.GetKey(KeyCode.Space))
+
+            gameObject.GetComponent<KeyPrompt>().Show();
+
+            if (this.gameObject.tag == "Platform")
             {
-                if (other.GetComponent<PlayerController>().totalInventory > 0)
+                Debug.Log("On the platform");
+                other.GetComponent<PlayerController>().onPlatform = true;
+                if (Input.GetKey(KeyCode.Space))
                 {
-                    float woodCollected = other.gameObject.GetComponent<PlayerController>().woodInventory;
-                    gameObject.GetComponent<BoatPlatformController>().woodCollected += woodCollected;
-                    other.gameObject.GetComponent<PlayerController>().woodInventory = 0;
+                    if (other.GetComponent<PlayerController>().totalInventory > 0)
+                    {
+                        float woodCollected = other.gameObject.GetComponent<PlayerController>().woodInventory;
+                        gameObject.GetComponent<BoatPlatformController>().woodCollected += woodCollected;
+                        other.gameObject.GetComponent<PlayerController>().woodInventory = 0;
 
-                    float ropeCollected = other.gameObject.GetComponent<PlayerController>().ropeInventory;
-                    gameObject.GetComponent<BoatPlatformController>().ropeCollected += ropeCollected;
-                    other.gameObject.GetComponent<PlayerController>().ropeInventory = 0;
+                        float ropeCollected = other.gameObject.GetComponent<PlayerController>().ropeInventory;
+                        gameObject.GetComponent<BoatPlatformController>().ropeCollected += ropeCollected;
+                        other.gameObject.GetComponent<PlayerController>().ropeInventory = 0;
 
-                    float foodCollected = other.gameObject.GetComponent<PlayerController>().foodInventory;
-                    gameObject.GetComponent<BoatPlatformController>().foodCollected += foodCollected;
-                    other.gameObject.GetComponent<PlayerController>().foodInventory = 0;
+                        float foodCollected = other.gameObject.GetComponent<PlayerController>().foodInventory;
+                        gameObject.GetComponent<BoatPlatformController>().foodCollected += foodCollected;
+                        other.gameObject.GetComponent<PlayerController>().foodInventory = 0;
 
-                    float clothCollected = other.gameObject.GetComponent<PlayerController>().clothInventory;
-                    gameObject.GetComponent<BoatPlatformController>().clothCollected += clothCollected;
-                    other.gameObject.GetComponent<PlayerController>().clothInventory = 0;
+                        float clothCollected = other.gameObject.GetComponent<PlayerController>().clothInventory;
+                        gameObject.GetComponent<BoatPlatformController>().clothCollected += clothCollected;
+                        other.gameObject.GetComponent<PlayerController>().clothInventory = 0;
 
-                    // Play a sound effect if we put *something* down
-                    if (woodCollected + ropeCollected + foodCollected + clothCollected > 0)
-                        Jukebox.Instance.PlaySFX("PutDownItem3");
+                        // Play a sound effect if we put *something* down
+                        if (woodCollected + ropeCollected + foodCollected + clothCollected > 0)
+                            Jukebox.Instance.PlaySFX("PutDownItem3");
+                    }
+
                 }
 
             }
 
-        }
-
-        {
-            if (Input.GetKey(KeyCode.E))
             {
-
-                if (this.gameObject.tag == "Food")
+                if (Input.GetKey(KeyCode.E))
                 {
-                    other.gameObject.GetComponent<PlayerController>().foodInventory +=1;
-                    Destroy(gameObject);
-                    Jukebox.Instance.PlaySFX("PickUpItem1");
 
+                    if (this.gameObject.tag == "Food")
+                    {
+                        other.gameObject.GetComponent<PlayerController>().foodInventory += 1;
+                        Destroy(gameObject);
+                        Jukebox.Instance.PlaySFX("PickUpItem1");
+
+                    }
+                    else if (this.gameObject.tag == "Wood")
+                    {
+                        other.gameObject.GetComponent<PlayerController>().woodInventory++;
+                        Destroy(gameObject);
+                        Jukebox.Instance.PlaySFX("PickUpItem1");
+
+                    }
+                    else if (this.gameObject.tag == "Rope")
+                    {
+                        other.gameObject.GetComponent<PlayerController>().ropeInventory += 1;
+                        Destroy(gameObject);
+                        Jukebox.Instance.PlaySFX("PickUpItem2");
+
+                    }
+                    else if (this.gameObject.tag == "Cloth")
+                    {
+                        other.gameObject.GetComponent<PlayerController>().clothInventory++;
+                        Destroy(gameObject);
+                        Jukebox.Instance.PlaySFX("PickUpItem2");
+
+                    }
+
+                    else if (this.gameObject.tag == "PowerUp")
+                    {
+                        other.gameObject.GetComponent<PlayerController>().SpeedBoost = true;
+                        Destroy(gameObject);
+                        Jukebox.Instance.PlaySFX("SpeedBoost");
+
+                    }
                 }
-                else if (this.gameObject.tag == "Wood")
-                {
-                    other.gameObject.GetComponent<PlayerController>().woodInventory++;
-                    Destroy(gameObject);
-                    Jukebox.Instance.PlaySFX("PickUpItem1");
 
-                }
-                else if (this.gameObject.tag == "Rope")
-                {
-                    other.gameObject.GetComponent<PlayerController>().ropeInventory+= 1;
-                    Destroy(gameObject);
-                    Jukebox.Instance.PlaySFX("PickUpItem2");
 
-                }
-                else if (this.gameObject.tag == "Cloth")
-                {
-                    other.gameObject.GetComponent<PlayerController>().clothInventory++;
-                    Destroy(gameObject);
-                    Jukebox.Instance.PlaySFX("PickUpItem2");
-
-                }
-
-                else if (this.gameObject.tag == "PowerUp")
-                {
-                    other.gameObject.GetComponent<PlayerController>().SpeedBoost = true;
-                    Destroy(gameObject);
-                    Jukebox.Instance.PlaySFX("SpeedBoost");
-
-                }
             }
-
-
         }
     }
 
