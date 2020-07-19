857bf015636235dd74cec0fe3a62ec95ce29ee59
Nikola <axzalar@gmail.com>
2020-07-19 16:58:05 +1200

    Animations

---
+++ Assets/Character/Animation/AnimScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimScript : MonoBehaviour
+{
+
+    Animator AnimCtrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimCtrl = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if movement key is pressed, swap state to running
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            AnimCtrl.SetInteger("Condition", 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            AnimCtrl.SetInteger("Condition", 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            AnimCtrl.SetInteger("Condition", 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            AnimCtrl.SetInteger("Condition", 1);
+        }
+
+        if (!Input.anyKey)
+        {
+            AnimCtrl.SetInteger("Condition", 0);
+        }
+
+    }
+}
